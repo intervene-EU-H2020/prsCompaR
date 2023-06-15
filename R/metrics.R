a6f020dca199e0a3bb57f017aab4cb4b64566aa0
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-06-15 16:32:07 +0100

    initial commit

---
+++ R/metrics.R
@@ -0,0 +1,16 @@
+#' Polygenic risk score performance predictive performance
+#'
+#' A subset of data from... (science stuff here)
+#'
+#' @format
+#'
+#' A data frame with 950 and 49 columns:
+#' \describe{
+#'   \item{predictor}{Country name}
+#'   \item{N}{Explanation..}
+#'   \item{h2l}{Explanation..}
+#'   \item{BETA_CI_LOW, BETA_CI_HIGH}{2 & 3 letter ISO country codes}
+#'   ...
+#' }
+#' @source <https://link/to/paper>
+"metrics"
